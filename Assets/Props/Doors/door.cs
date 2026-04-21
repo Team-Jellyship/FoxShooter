bb5ee2417b9d520807103af47796e9b9ae67f886
Tiller99c <tylerforrester9910@gmail.com>
2026-04-21 15:56:45 -0400

    DOOR STUCK
    I got the door working using the old control scheme, doesn't look like I want it to either. Thursday I will edit the animation and attach it to the proper control scheme

---
+++ Assets/Props/Doors/door.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+public class door : MonoBehaviour
+{
+    public float interactionDistance;
+    public GameObject intText;
+    public string doorOpenAnimName, doorCloseAnimName;
+
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, interactionDistance))
+        {
+            if(hit.collider.gameObject.tag == "door")
+            {
+                GameObject doorParent = hit.collider.transform.root.gameObject;
+                Animator doorAnim = doorParent.GetComponent<Animator>();
+                intText.SetActive(true);
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                    if (doorAnim.GetCurrentAnimatorStateInfo(0).IsName(doorOpenAnimName))
+                    {
+                        doorAnim.ResetTrigger("open");
+                        doorAnim.SetTrigger("close");
+                    }
+                    if (doorAnim.GetCurrentAnimatorStateInfo(0).IsName(doorCloseAnimName))
+                    {
+                        doorAnim.ResetTrigger("close");
+                        doorAnim.SetTrigger("open");
+                    }
+                }
+                return;
+            }
+            else
+            {
+                intText.SetActive(false);
+            }
+        }
+    }
+}
