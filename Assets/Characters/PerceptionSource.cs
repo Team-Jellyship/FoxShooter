29c483af5c707d153156c8f0e5b0a9c97696c0c4
Stephen Melnick <stephenwmelnick@gmail.com>
2026-04-21 00:20:17 -0400

    Added vision cones and logic to behaviour graph

---
+++ Assets/Characters/PerceptionSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using FoxShooter.Game;
+using UnityEngine;
+
+namespace FoxShooter.Characters
+{
+    public class PerceptionSource : MonoBehaviour
+    {
+        private void Start()
+        {
+            PerceptionSubsystem.instance.RegisterPerceptionSource(this);
+        }
+    }
+}
