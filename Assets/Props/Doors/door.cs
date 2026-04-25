b5beb395afb937a5e85ab9f411b4c8aeea79d492
Tiller99c <tylerforrester9910@gmail.com>
2026-04-25 15:09:21 -0400

    Open Na Noor
    Converted back to the proper Control Scheme, Door now auto opens when being lookced at and closes automatically

--- Assets/Props/Doors/door.cs
+++ Assets/Props/Doors/door.cs
@@ -5,40 +5,73 @@ using UnityEngine.InputSystem;
 
 public class door : MonoBehaviour
 {
+
     public float interactionDistance;
     public GameObject intText;
     public string doorOpenAnimName, doorCloseAnimName;
+    public int state = 0;
+    public CharacterController charCtrl;
+
+    void Start()
+    {
+        charCtrl = GetComponent<CharacterController>();
+    }
 
     void Update()
     {
+
+        
         Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
-        if(Physics.Raycast(ray, out hit, interactionDistance))
+        //if(Physics.OverlapSphere(transform.position, interactionDistance + 3))
+        //{
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, interactionDistance + 3);
+        foreach (var hitCollider in hitColliders)
         {
-            if(hit.collider.gameObject.tag == "door")
+            GameObject doorParent = hitCollider.GetComponent<Collider>().transform.root.gameObject;
+            Animator doorAnim = doorParent.GetComponent<Animator>();
+            if (hitCollider.tag == "door")
             {
-                GameObject doorParent = hit.collider.transform.root.gameObject;
-                Animator doorAnim = doorParent.GetComponent<Animator>();
-                intText.SetActive(true);
-                if(Input.GetKeyDown(KeyCode.E))
-                {
-                    if (doorAnim.GetCurrentAnimatorStateInfo(0).IsName(doorOpenAnimName))
-                    {
-                        doorAnim.ResetTrigger("open");
-                        doorAnim.SetTrigger("close");
-                    }
-                    if (doorAnim.GetCurrentAnimatorStateInfo(0).IsName(doorCloseAnimName))
-                    {
-                        doorAnim.ResetTrigger("close");
-                        doorAnim.SetTrigger("open");
-                    }
-                }
-                return;
-            }
-            else
-            {
-                intText.SetActive(false);
+                doorAnim.ResetTrigger("open");
+                doorAnim.SetTrigger("close");
             }
         }
+        //}
+        //if (Physics.SphereCast(transform.position, 7, transform.forward, out hit, interactionDistance+3))
+       // {
+         //   GameObject doorParent = hit.collider.transform.root.gameObject;
+          //  Animator doorAnim = doorParent.GetComponent<Animator>();
+         //   if ((hit.collider.gameObject.tag == "door"))
+         //   {
+         //       doorAnim.ResetTrigger("open");
+         //       doorAnim.SetTrigger("close");
+         //   }
+
+
+        //}
+
+        //Sphere sphere = new Sphere(transform.position, interactionDistance);
+        //if (Physics.CheckSphere(transform.position, interactionDistance))
+        //{
+        //    Debug.Log("Hello");
+        //}
+        if (Physics.Raycast(ray, out hit, interactionDistance))
+        //if (Physics.SphereCast(transform.position, 2, transform.forward, out hit, interactionDistance))
+        {
+            GameObject doorParent = hit.collider.transform.root.gameObject;
+            Animator doorAnim = doorParent.GetComponent<Animator>();
+            if (hit.collider.gameObject.tag == "door")
+            {
+                doorAnim.ResetTrigger("close");
+                doorAnim.SetTrigger("open");
+                //if (doorAnim.GetCurrentAnimatorStateInfo(0).IsName(doorOpenAnimName))
+            }
+        }
+        else
+        {
+            //doorAnim.ResetTrigger("open");
+            //doorAnim.SetTrigger("close");
+        }
     }
+
 }
