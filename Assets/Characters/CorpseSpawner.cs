0cc93642feaf502c64b5df71f2920f3ca20341ec
Stephen Melnick <stephenwmelnick@gmail.com>
2026-05-01 02:08:56 -0400

    Implemented sprites and fixes for Claw enemy

---
+++ Assets/Characters/CorpseSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace FoxShooter.Characters
+{
+    public class CorpseSpawner : MonoBehaviour
+    {
+        // Object to copy transforms from
+        [SerializeField] private GameObject corpseSpriteObject;
+        [SerializeField] private Sprite sprite;
+        
+        public void Spawn()
+        {
+            var corpse = Instantiate(corpseSpriteObject);
+            corpse.transform.position = corpseSpriteObject.transform.position;
+            corpse.name = $"{name}_Corpse";
+            corpse.GetComponent<SpriteRenderer>().sprite = sprite;
+            Destroy(gameObject);
+        }
+    }
+}
